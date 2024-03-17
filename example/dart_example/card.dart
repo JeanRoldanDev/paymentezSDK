08c70ddd5e6f3844d5488329369e95db9d7ddeb9
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2024-03-17 16:24:26 -0500

    chore: comment example

--- example/dart_example/card.dart
+++ example/dart_example/card.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:paymentez_sdk/paymentez_sdk.dart';
 
 import 'env.dart';
@@ -20,52 +18,52 @@ Future<void> main(List<String> args) async {
   //   isPCI: true,
   // );
 
-  const userID = '5a9b9072-4d60-4846-be71-d533d3851901';
+  // const userID = '5a9b9072-4d60-4846-be71-d533d3851901';
 
-  log('========= GET CARDS =========');
-  final (cardsResponse, paymentezError) = await sdk.getAllCards(userID);
-  if (paymentezError != null) {
-    log(paymentezError.toJson().toString());
-    return;
-  }
+  // log('========= GET CARDS =========');
+  // final (cardsResponse, paymentezError) = await sdk.getAllCards(userID);
+  // if (paymentezError != null) {
+  //   log(paymentezError.toJson().toString());
+  //   return;
+  // }
 
-  log(cardsResponse!.toJson().toString());
-  if (cardsResponse.resultSize == 0) {
-    log('========= ADD CARD =========');
-    final addCardRequest = AddCardRequest(
-      user: UserCard(
-        id: userID,
-        email: 'test@example.com',
-      ),
-      card: NewCard(
-        number: '4111111111111111',
-        holderName: 'citlali calderon',
-        expiryMonth: 9,
-        expiryYear: 2025,
-        cvc: '123',
-      ),
-    );
+  // log(cardsResponse!.toJson().toString());
+  // if (cardsResponse.resultSize == 0) {
+  //   log('========= ADD CARD =========');
+  //   final addCardRequest = AddCardRequest(
+  //     user: UserCard(
+  //       id: userID,
+  //       email: 'test@example.com',
+  //     ),
+  //     card: NewCard(
+  //       number: '4111111111111111',
+  //       holderName: 'citlali calderon',
+  //       expiryMonth: 9,
+  //       expiryYear: 2025,
+  //       cvc: '123',
+  //     ),
+  //   );
 
-    final (addCardResponse, paymentezError) = await sdk.addCard(addCardRequest);
-    if (paymentezError != null) {
-      log(paymentezError.toJson().toString());
-      return;
-    } else {
-      log(addCardResponse!.toJson().toString());
-    }
-  } else {
-    log('========= DELECTE CARD =========');
-    final deleteCardRequest = DeleteCardRequest(
-      cardToken: cardsResponse.cards.first.token,
-      userId: userID,
-    );
-    final (deleteCardResponse, paymentezError) =
-        await sdk.deleteCard(deleteCardRequest);
-    if (paymentezError != null) {
-      log(paymentezError.toJson().toString());
-      return;
-    } else {
-      log(deleteCardResponse!.toJson().toString());
-    }
-  }
+  //   final (addCardResponse, paymentezError) = await sdk.addCard(addCardRequest);
+  //   if (paymentezError != null) {
+  //     log(paymentezError.toJson().toString());
+  //     return;
+  //   } else {
+  //     log(addCardResponse!.toJson().toString());
+  //   }
+  // } else {
+  //   log('========= DELECTE CARD =========');
+  //   final deleteCardRequest = DeleteCardRequest(
+  //     cardToken: cardsResponse.cards.first.token,
+  //     userId: userID,
+  //   );
+  //   final (deleteCardResponse, paymentezError) =
+  //       await sdk.deleteCard(deleteCardRequest);
+  //   if (paymentezError != null) {
+  //     log(paymentezError.toJson().toString());
+  //     return;
+  //   } else {
+  //     log(deleteCardResponse!.toJson().toString());
+  //   }
+  // }
 }
