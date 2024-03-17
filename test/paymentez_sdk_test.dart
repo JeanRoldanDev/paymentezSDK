265c3602241dfdfd4f5532b68794615b8c843cee
antonio1612 <agiler1699@gmail.com>
2024-03-17 18:47:23 -0500

    chore: commented models and tests

--- test/paymentez_sdk_test.dart
+++ test/paymentez_sdk_test.dart
@@ -1,43 +1,43 @@
-import 'package:paymentez_sdk/paymentez_sdk.dart';
-import 'package:test/test.dart';
+// import 'package:paymentez_sdk/paymentez_sdk.dart';
+// import 'package:test/test.dart';
 
-void main() {
-  late PaymentezSDK sdk;
+// void main() {
+//   late PaymentezSDK sdk;
 
-  setUp(() {
-    sdk = PaymentezSDK();
-  });
-  test('SDK instance test call getAllCards', () async {
-    final (resp, err) = await sdk.getAllCards('4');
-    expect(resp, isNull);
-    expect(err, isA<PaymentezError>());
-  });
+//   setUp(() {
+//     sdk = PaymentezSDK();
+//   });
+//   test('SDK instance test call getAllCards', () async {
+//     final (resp, err) = await sdk.getAllCards('4');
+//     expect(resp, isNull);
+//     expect(err, isA<PaymentezError>());
+//   });
 
-  test('SDK instance test call addCard', () async {
-    final (resp, err) = await sdk.addCard(
-      AddCardRequest(
-        user: UserCard(id: 'mockData', email: 'mockData'),
-        card: NewCard(
-          number: 'mockData',
-          holderName: 'mockData',
-          expiryMonth: 09,
-          expiryYear: 2025,
-          cvc: 'mockData',
-        ),
-      ),
-    );
-    expect(resp, isNull);
-    expect(err, isA<PaymentezError>());
-  });
+//   test('SDK instance test call addCard', () async {
+//     final (resp, err) = await sdk.addCard(
+//       AddCardRequest(
+//         user: UserCard(id: 'mockData', email: 'mockData'),
+//         card: NewCard(
+//           number: 'mockData',
+//           holderName: 'mockData',
+//           expiryMonth: 09,
+//           expiryYear: 2025,
+//           cvc: 'mockData',
+//         ),
+//       ),
+//     );
+//     expect(resp, isNull);
+//     expect(err, isA<PaymentezError>());
+//   });
 
-  test('SDK instance test call getAllCards', () async {
-    final (resp, err) = await sdk.deleteCard(
-      DeleteCardRequest(
-        cardToken: 'mockData',
-        userId: 'mockData',
-      ),
-    );
-    expect(resp, isNull);
-    expect(err, isA<PaymentezError>());
-  });
-}
+//   test('SDK instance test call getAllCards', () async {
+//     final (resp, err) = await sdk.deleteCard(
+//       DeleteCardRequest(
+//         cardToken: 'mockData',
+//         userId: 'mockData',
+//       ),
+//     );
+//     expect(resp, isNull);
+//     expect(err, isA<PaymentezError>());
+//   });
+// }
