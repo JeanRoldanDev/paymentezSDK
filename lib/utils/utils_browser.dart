080a215caff5a335305a21e4b058f5e8cc345fd4
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2024-03-18 10:31:10 -0500

    chore: add onListenerResultSaveCard and refactor code

--- lib/utils/utils_browser.dart
+++ lib/utils/utils_browser.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 class UtilsBrowser {
   UtilsBrowser({this.isProd = false});
 
@@ -9,11 +7,75 @@ class UtilsBrowser {
       isProd ? 'ccapi.paymentez.com' : 'ccapi-stg.paymentez.com';
 
   String get onCallbackSaveCard {
-    log('get onCallballSaveCard');
     final javascript = '''
-        let message = { type: "tokenize", data: null };
-        window.postMessage(message, "https://$_host");
+        window.postMessage({type: "tokenize", data: null}, "https://$_host");
       ''';
     return javascript;
+  }
+
+  String get onListenerResultSaveCard {
+    return '''
+      window.addEventListener("message", (event) => {
+        if (event.origin !== "https://$_host") {
+          return;
+        }
+
+        const msg = event.data;
+        switch (msg.type) {
+          case "incomplete_form":
+            console.log("SDK_PAYMENTEZ: incomplete_form " + msg.data);
+
+            const dataErr = {
+              type: "incomplete_form",
+              data: null,
+              message: msg.data,
+            };
+
+            if (window.flutter_inappwebview !== undefined) {
+              window.flutter_inappwebview
+                .callHandler("SendDataSDK", dataErr)
+                .then(function (result) {
+                  console.log(
+                    "SDK_PAYMENTEZ: Data successfully sent to the SDK =>" +
+                      result
+                  );
+                })
+                .catch(function (error) {
+                  console.log(
+                    "SDK_PAYMENTEZ: Error getting data from WebView => " + error
+                  );
+                });
+            }
+            break;
+          case "tokenize_response":
+            console.log(
+              "SDK_PAYMENTEZ: tokenize_response " + JSON.stringify(msg.data)
+            );
+
+            const data = {
+              type: "tokenize_response",
+              data: msg.data,
+              message: msg.data.message,
+            };
+
+            if (window.flutter_inappwebview !== undefined) {
+              window.flutter_inappwebview
+                .callHandler("SendDataSDK", data)
+                .then(function (result) {
+                  console.log(
+                    "SDK_PAYMENTEZ: Data successfully sent to the SDK =>" +
+                      result
+                  );
+                })
+                .catch(function (error) {
+                  console.log(
+                    "SDK_PAYMENTEZ: Error getting data from WebView => " + error
+                  );
+                });
+            }
+            break;
+        }
+      });
+        ''';
   }
 }
