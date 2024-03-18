b6a7ed0b54b5f20a021dd93c6987dc8792baf3b3
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2024-03-18 11:08:23 -0500

    chore: update PageWebView

--- example/app_example/lib/page_webview.dart
+++ example/app_example/lib/page_webview.dart
@@ -1,33 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:paymentez_sdk/paymentez_controller.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class PageWebView extends StatefulWidget {
   const PageWebView({
     required this.url,
+    required this.paymentezCtrl,
     super.key,
   });
 
   final String url;
+  final PaymentezController paymentezCtrl;
 
   @override
   State<PageWebView> createState() => _PageWebViewState();
 }
 
 class _PageWebViewState extends State<PageWebView> {
-  late WebViewController _ctrl = WebViewController();
+  late WebViewController _ctrl;
 
   @override
   void initState() {
-    _ctrl = WebViewController();
-    _ctrl.loadRequest(Uri.parse(widget.url));
+    _ctrl = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.blue)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (String url) {
+            if (widget.url.isNotEmpty) {
+              widget.paymentezCtrl.onInit();
+            }
+          },
+        ),
+      )
+      ..setOnConsoleMessage((message) {
+        print('MESAJE CONSOLE');
+        print(message.message);
+      });
+    widget.paymentezCtrl.setController(_ctrl);
     super.initState();
   }
 
+  //  _ctrl.runJavaScript(widget.paymentezCtrl.utils.onCallbackSaveCard);
+
   @override
   Widget build(BuildContext context) {
-    return WebViewWidget(
-      key: Key('${widget.url}${DateTime.now().millisecondsSinceEpoch}'),
-      controller: _ctrl,
+    if (widget.url.isNotEmpty) {
+      _ctrl.loadRequest(Uri.parse(widget.url));
+    }
+    return SizedBox.expand(
+      child: WebViewWidget(
+        key: Key('${widget.url}${DateTime.now().millisecondsSinceEpoch}'),
+        controller: _ctrl,
+      ),
     );
   }
 }
